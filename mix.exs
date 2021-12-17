ee4c0113368c2b8d4b69b6868202ad08d83fb7b8
stefkohub <ste.falsetto@gmail.com>
2021-12-17 12:05:57 +0100

    Added some configuration for ElxirCI

--- mix.exs
+++ mix.exs
@@ -9,7 +9,28 @@ defmodule DarknetToOnnx.MixProject do
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
       escript: [main_module: DarknetToOnnx.CLI],
-      deps: deps()
+      deps: deps(),
+####### From ElixirCI
+      build_path: "_build",
+      deps_path: "deps",
+      lockfile: "mix.lock",
+      consolidate_protocols: Mix.env() != :test,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_add_apps: [:elixir, :mix],
+        flags: [
+          :error_handling,
+          :race_conditions,
+          :underspecs
+        ],
+        ignore_warnings: ".dialyzer_ignore.exs"
+      ]
+############
     ]
   end
 
@@ -26,6 +47,9 @@ defmodule DarknetToOnnx.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       # {:configparser_ex, git: "https://github.com/easco/configparser_ex", tag: "master"},
+      {:credo, "~> 1.5.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.14", only: [:dev, :test], runtime: false},
       {:configparser_ex, git: "https://github.com/stefkohub/configparser_ex", tag: "master"},
       {:axon, "~> 0.1.0-dev", github: "elixir-nx/axon"},
       {:axon_onnx, github: "stefkohub/axon_onnx", tag: "master"},
